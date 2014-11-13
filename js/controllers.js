4eb749524b958a426f9dcbf9bd00ad655372344c
sebastianpennino <sebastian.pennino@globant.com>
2014-11-13 01:50:06 -0300

    Refactor to revealing module

--- js/controllers.js
+++ js/controllers.js
@@ -1,148 +1,123 @@
-( function( window, MG, undefined ) {
-  "use strict";
-  
-  function CardsController() {
+myMemGame.CardsController = (function (window, MG, undefined) {
+    "use strict";
 
-    this.shuffleIt = function shuffleIt(){
-      var targetEl = document.getElementById( 'deck' ),
-          ul = MG.utils.shuffleDOM(targetEl);
-      $('#deck').html('').append(ul);
-    };
-
-    this.newGame = function newGame(){
-      $('#deck').find('.card').removeClass('founded flipped');
-      this.shuffleIt();
-    };
-
-    this.checkWinCondition = function checkWinCondition(){
-      var winCondition = $('#deck').find('.card.founded').length === 30;
-      if(winCondition){
-        alert('Congratulations!!! A new game will begin in 5 seconds...');
-        setTimeout(  this.newGame.bind(this) , 5000 );
+    var props = {
+      cardHistory : [],   // Will store player moves, for future stats
+      playerStats : {
+        pairs        : 0, // Number of pairs
+        misses       : 0, // Number of misses
+        streak       : 0, // Continuous matchs
+        inStreak     : 0, // Currently in a streak?
+        maxStreak    : 0  // Max score of steak
+      },
+      selectors : {
+        main : document.getElementById('deck')
+      },
+      oldcard : {
+        cardName : null,  // comparison used by compareCard
+        unique   : null   // comparison used by compareCard
       }
-    }
+    },
+    methods = {
+      // all methods will be defined later
+    };
 
-    this.init = function init() {
+    methods.shuffleIt = function () {
+      var deck = props.selectors.main,
+            ul = MG.utils.shuffleDOM( deck );
+      // $('#deck').html('').append(ul);
+      deck.innerHTML = '';
+      deck.appendChild(ul);
+    };
 
-      this.shuffleIt();
+    methods.newGame = function () {
+      ///props.selectors.main.querySelectorAll('.card')
+      $('#deck').find('.card').removeClass('founded flipped');
+      methods.shuffleIt();
+    };
 
-      var targetEl = document.getElementById( 'deck' );
+    methods.checkWinCondition = function () {
+      //var win = $('#deck').find('.card.founded').length === 30;
+      var win = props.selectors.main.querySelectorAll('.card.founded').length === 30;
+      if(win){
+        alert('Congratulations!!! A new game will begin in 5 seconds...');
+        setTimeout(  methods.newGame.bind(this) , 5000 );
+      }
+    };
+
+    methods.initialize = function () {
+      methods.shuffleIt();
       // Adding event handlers
-      MG.utils.addEvent(targetEl, "click", function(evt){
-        // Test for the maximum number of revealed cards
+      MG.utils.addEvent( props.selectors.main , "click", function(evt){
         evt.preventDefault();
-
-        console.log(  evt.target.nodeName  );
+        // Basic event delegation
         if(evt.target && evt.target.nodeName == "A"){
-          if(this.checkForReveals() === true){
-            this.selectCard(evt);
-            this.stats();
-            this.checkWinCondition();
+          // If 2 or less cards are revealed
+          if(methods.checkForReveals() === true){
+            methods.selectCard(evt);
+            //methods.stats();
+            methods.checkWinCondition();
           }
         }
 
       }.bind(this) );
     };
 
-    this.checkForReveals = function checkForReveals(){
-      var flips = $('#deck').find('.card.flipped').not('.founded').length;
-      if( flips >= 2 ) {
-        return false;
-      } else {
-        return true;
-      }
+    methods.checkForReveals = function () {
+      var flips = $( props.selectors.main ).not('.founded').length;
+      return flips >= 2 ? false : true ;
     };
 
-    var cardHistory = [];
-    //var cardCache = [];
-    var playerStats = {
-      pairs        : 0, // Number of pairs
-      misses       : 0, // Number of misses
-      streak       : 0, // Continuous matchs
-      inStreak     : 0, // Currently in a streak?
-      maxStreak    : 0  // Max score of steak
-    };
-    
-    this.stats = function stats(){
-      console.log( playerStats );
+    methods.stats = function () {
+      console.log( props.playerStats );
     };
 
-    this.deselectAllFlippedCards = function(){
-       $('#deck').find('.card.flipped').not('.founded').removeClass('flipped');
+    methods.deselectAllFlippedCards = function  () {
+      $('#deck').find('.flipped').not('.founded').removeClass('flipped');
     };
 
-    this.compareCard = function compareCard( cardName, cardId ){
-      cardHistory.push( cardId+':'+cardName );
+    methods.compareCard = function ( cardName, cardId ){
+      var oldcard = props.oldcard;
+      props.cardHistory.push( cardId+':'+cardName );
 
       if(oldcard.cardName === null){
         oldcard.cardName = cardName;
         oldcard.unique = cardId;
+
       } else {
+
         if(oldcard.cardName === cardName){
-          alert('Good Match!');
-          /*
-          playerStats.pairs++;
-          if(playerStats.inStreak){
-            playerStats.streak++;
-            if(playerStats.streak < 3){
-              alert('Excelent Match, you are in a streak of '+playerStats.streak+'!');
-            } else {
-              alert('You are on fire! (Streak of '+playerStats.streak+'!)');
-            }
-          } else {
-            alert('Good Match!');
-            playerStats.streak++;
-            playerStats.inStreak = 1;
-          }
-          */
-          $('#deck').find('.card.flipped').not('.founded').addClass('founded');
+          alert('Good Catch!');
+          $('#deck').find('.flipped').not('.founded').addClass('founded');
         } else {
           alert('Sorry, try again!');
-          /*
-          if(playerStats.streak > playerStats.maxStreak) {
-            playerStats.maxStreak = playerStats.streak;
-            alert('What a bummer, that was your max streak until now. ('+playerStats.maxStreak+' hits in a row)');
-          }
-          playerStats.misses++;
-          playerStats.inStreak = 0;
-          playerStats.streak = 0;
-          */
-          setTimeout(  this.deselectAllFlippedCards.bind(this) , 200 );
+          window.setTimeout(  methods.deselectAllFlippedCards.bind(this) , 200 );
         }
+
         oldcard.cardName = null;
         oldcard.unique = null;
       }
     };
 
-
-    var oldcard = {
-      cardName : null,
-      unique : null
-    };
-
-    this.selectCard = function selectCard(evt) {
-      var el = evt.target,
+    methods.selectCard = function (evt) {
+      var el       = evt.target,
+          oldcard  = props.oldcard,
           cardName = $(el.parentNode).data('cardName'),
-          cardId = $(el.parentNode).data('cardUnique');
+          cardId   = $(el.parentNode).data('cardUnique');
 
       if(cardId !== oldcard.unique){
         $(el.parentNode).addClass('flipped');
-        this.compareCard(cardName, cardId);
-        console.log(cardName, cardId);
-      } else {
-        console.log('same card clicked!');
+        methods.compareCard(cardName, cardId);
       }
-      
+
     };
 
-    
-  }
-  
-  // expose access to the constructor
-  window.CardsController = CardsController;
-  
-} )( window, myMemGame );
- 
-// example usage
-var cardsController = new CardsController();
-cardsController.init();
+    // Exposed methods
+    return {
+        init: methods.initialize
+    };
+
+})(window, myMemGame || {} );
+
+// Initialize
+myMemGame.CardsController.init();
